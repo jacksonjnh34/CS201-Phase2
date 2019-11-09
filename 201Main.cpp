7fd596d96d3816746a9f504d270bedb17e1ff3dd
Jackson Haile <jacksonjnh35@gmail.com>
2019-11-09 04:34:57 +0000

    Updated file name and completed insert, order prints, rank (somewhat) and search'

--- 201Main.cpp
+++ 201Main.cpp
@@ -8,7 +8,14 @@ int main(){
 	
 	RBTree<string,int> T1, T2(K,V,10);
 	
-	for(int i=0; i<10; i++) T1.insert(K[i],V[i]);
+	cout << endl << endl << "--------------------------------------------------------" << endl;
+
+	for(int i=0; i<10; i++)
+	{
+		cout << "Inserting node" << i+1 << ", with key: " << K[i] << " and value: " << V[i] << endl;
+		T1.insert(K[i],V[i]);
+		cout << endl;
+	} 
 	// T1 and T2 should be identical trees
 	
 	cout << *(T2.search("C")) << endl;
@@ -17,7 +24,7 @@ int main(){
 	cout << T1.rank("C") << endl;
 	//Should output 3
 	
-	cout << T1.select(3) << endl;
+	//cout << T1.select(3) << endl;
 	//Should output C 
 	
 	T2.preorder();
@@ -26,10 +33,10 @@ int main(){
 	T2.inorder();
 	//Should output	A B C D E F H I J K
 	
-	cout << T2.remove("G") << endl;
+	//cout << T2.remove("G") << endl;
 	//Should output 0
 	
-	cout << T2.remove("B") << endl;
+	//cout << T2.remove("B") << endl;
 	//Should output 1
 	
 	cout <<T2.rank("J") << endl;
@@ -38,18 +45,20 @@ int main(){
 	T2.inorder();
 	//Should output	A C D E F H I J K
 	
-	RBTree<string,int> L, R;
-	T1.split("G", L, R);
-	cout << R.rank("K") << endl;
+	//RBTree<string,int> L, R;
+	//T1.split("G", L, R);
+	//cout << R.rank("K") << endl;
 	// Should output 4
 	
+	/*
     RBTree<int,int> X;
 	for (int i=1;i<100000;i++) X.insert(i,i);
 	for (int i=1;i<100000;i++) {
 		if(X.rank(i) != i) cout << "Rank error" << endl;
-		if(X.select(i) != i) cout << "Select error" << endl;
+		//if(X.select(i) != i) cout << "Select error" << endl;
 		if(*(X.search(i)) != i) cout << "Search error" << endl;
 	}  
 	//Should be no output and should take seconds, not minutes
+	*/
 	return 0;
 }
